dc586253a0f6cd28c7f887f41c3c156af2653572
delicjasmin <delicjasmi@gmail.com>
2024-01-23 15:51:02 +0100

    Added environment variables

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,11 +2,16 @@ import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from "mysql2"
 
 export const poolConnection = mysql.createPool({
-  "host": "127.0.0.1",
-  "port": 3306,
-  "database": "jaskich",
-  "user": "root",
-  "password": "2580"
+  // "host": "127.0.0.1",
+  // "port": 3306,
+  // "database": "jaskich",
+  // "user": "root",
+  // "password": "2580"
+  "host": process.env.DB_HOST,
+  "port": parseInt(process.env.DB_PORT),
+  "database": process.env.DB_DATABASE,
+  "user": process.env.DB_USER,
+  "password": process.env.DB_PASSWORD
 })
 
 export const db = drizzle(poolConnection);
